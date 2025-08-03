d52dd77b7d7dce18345c330ddf21a1c00438ff88
grqphical07 <95062977+grqphical07@users.noreply.github.com>
2025-08-03 16:26:36 -0700

    made root a flex container for easier layouts

--- frontend/src/Components/Hero.tsx
+++ frontend/src/Components/Hero.tsx
@@ -1,7 +1,9 @@
+import { NavLink } from "react-router";
+
 function Hero() {
   return (
     <>
-      <section className="bg-gray-900 text-white min-h-screen flex items-center justify-center px-6">
+      <section className="bg-gray-900 text-white flex items-center justify-center px-6 grow">
         <div className="max-w-2xl text-center">
           <h1 className="text-5xl font-bold mb-4">
             NHL Goaltending Analytics{" "}
@@ -9,12 +11,18 @@ function Hero() {
           </h1>
           <p className="text-lg mb-6 text-gray-300"></p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
-            <button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-xl transition cursor-pointer">
-              Get Started
-            </button>
-            <button className="bg-white text-gray-900 px-6 py-3 rounded-xl hover:bg-gray-200 transition cursor-pointer">
-              Learn More
-            </button>
+            <NavLink
+              to="/goalies"
+              className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-xl transition cursor-pointer"
+            >
+              View The Stats
+            </NavLink>
+            <NavLink
+              to="/playground"
+              className="bg-white text-gray-900 px-6 py-3 rounded-xl hover:bg-gray-200 transition cursor-pointer"
+            >
+              Use our Models
+            </NavLink>
           </div>
         </div>
       </section>
