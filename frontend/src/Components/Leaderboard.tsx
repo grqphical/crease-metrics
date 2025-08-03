b78e50d650b1b67be16b8b6b0e75c910554e9210
grqphical07 <95062977+grqphical07@users.noreply.github.com>
2025-08-03 16:55:31 -0700

    added loading spinner to appear while leaderboard data loads

--- frontend/src/Components/Leaderboard.tsx
+++ frontend/src/Components/Leaderboard.tsx
@@ -1,5 +1,6 @@
 import Box from "./Box";
 import Icon from "./Icon";
+import Loader from "./Loader";
 
 export interface Name {
   default: string;
@@ -34,50 +35,56 @@ function Leaderboard(props: LeaderboardProps) {
     }
   };
 
+  const renderTable = () => {
+    return (
+      <table className="w-full text-sm text-left">
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th>Name</th>
+            <th>{props.metric}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.leaderboard.map((value, index) => {
+            return (
+              <tr key={index}>
+                <td>
+                  <img
+                    src={value.headshot}
+                    alt=""
+                    className="w-16 h-16 rounded-full"
+                  />
+                </td>
+
+                <td className="text-xl">
+                  <Icon team={value.teamAbbrev} />
+                </td>
+                <td>
+                  <p className="text-xl">
+                    {value.firstName.default} {value.lastName.default}
+                  </p>
+                </td>
+                <td>
+                  <p className="text-xl font-bold">
+                    {renderValue(value.value)}
+                  </p>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  };
+
   return (
     <>
       <Box>
         <h1 className="text-3xl">{props.title}</h1>
         <div className="relative overflow-x-auto pt-3">
-          <table className="w-full text-sm text-left">
-            <thead>
-              <tr>
-                <th></th>
-                <th></th>
-                <th>Name</th>
-                <th>{props.metric}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {props.leaderboard.map((value, index) => {
-                return (
-                  <tr key={index}>
-                    <td>
-                      <img
-                        src={value.headshot}
-                        alt=""
-                        className="w-16 h-16 rounded-full"
-                      />
-                    </td>
-
-                    <td className="text-xl">
-                      <Icon team={value.teamAbbrev} />
-                    </td>
-                    <td>
-                      <p className="text-xl">
-                        {value.firstName.default} {value.lastName.default}
-                      </p>
-                    </td>
-                    <td>
-                      <p className="text-xl font-bold">
-                        {renderValue(value.value)}
-                      </p>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          {props.leaderboard.length === 0 ? <Loader /> : renderTable()}
         </div>
       </Box>
     </>
