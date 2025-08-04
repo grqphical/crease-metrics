af632699b53092ff56bb11f3d125be5972bc7764
grqphical07 <95062977+grqphical07@users.noreply.github.com>
2025-08-03 20:08:54 -0700

    added api proxies to go server

--- backend/api/main.go
+++ backend/api/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+func CreateProxyHandler(key string) func(*gin.Context) {
+	return func(c *gin.Context) {
+		resp, err := http.DefaultClient.Get("https://api-web.nhle.com/v1/goalie-stats-leaders/current?limit=5")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		var data map[string]any
+		err = json.NewDecoder(resp.Body).Decode(&data)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, data[key])
+	}
+
+}
+
 func main() {
-	server := http.NewServeMux()
+	router := gin.Default()
+
+	api := router.Group("/api")
+	api.GET("/leaderboard/wins", CreateProxyHandler("wins"))
+	api.GET("/leaderboard/svpct", CreateProxyHandler("savePctg"))
+	api.GET("/leaderboard/gaa", CreateProxyHandler("goalsAgainstAverage"))
 
 	fmt.Println("API running on 127.0.0.1:8000")
-	http.ListenAndServe("127.0.0.1:8000", server)
+	router.Run("127.0.0.1:8000")
 }
